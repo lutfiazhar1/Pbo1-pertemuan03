b438a1e3b2319ab2855a94f6795afb5cc9a8d01d
Lutfi Azhar <lutfiazhr12@gmail.com>
2020-04-14 06:32:26 +0700

    Condisi true

---
+++ src/Percabangan/Ifelse.java
@@ -0,0 +1,14 @@
+package Percabangan;
+
+public class Ifelse {
+    public static void main(String[] args){
+        double totalbelanja = 53000;
+        double uangdompet = 25000;
+
+        if (uangdompet<totalbelanja){
+            System.out.println("Uang Anda Kurang");
+        }else {
+            System.out.println("Belanja Berhasil , Selamat menikmati");
+        }
+    }
+}
