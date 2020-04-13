153cf3f6ad647c3caffb218731e5d936ee4ab84d
Lutfi Azhar <lutfiazhr12@gmail.com>
2020-04-14 05:51:01 +0700

    Operator Binary Assigment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package Operator;
+
+public class OperatorBinary {
+ //operator (=) Sebagai Penugasan
+    //Menempatkan Nilai dalam Varible
+    //baris 5-7 adalah coment/ yang tidak dijalanakan oleh program
+
+    String nama ="Lutfi Azhar";
+    int jumlahSks = 24;
+    double indekPrestasiKumulatif = 3.89;
+
+
+}
