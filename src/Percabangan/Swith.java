f7ab717ec7301b32550e77456421d9a53dabbe94
Lutfi Azhar <lutfiazhr12@gmail.com>
2020-04-14 06:48:12 +0700

    pecabangan Switch

---
+++ src/Percabangan/Swith.java
@@ -0,0 +1,24 @@
+package Percabangan;
+
+public class Swith {
+    public static void main(String[] args) {
+        int jalanjalan = 3;
+        switch (jalanjalan){
+            case 1:
+                System.out.println("uranglapas");
+                break;
+            case 2:
+                System.out.println("urang ancaan");
+                break;
+            case 3:
+                System.out.println("ubat nyamuk");
+                break;
+            case 4:
+                System.out.println("dubil adit");
+                break;
+            case 5:
+                System.out.println("mahluk halus");
+                break;
+        }
+    }
+}
