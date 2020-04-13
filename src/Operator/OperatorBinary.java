bce3102549feeaa994c329c36d78f64bc94809cd
Lutfi Azhar <lutfiazhr12@gmail.com>
2020-04-14 06:00:08 +0700

    Operator Binary Aritmatika

--- src/Operator/OperatorBinary.java
+++ src/Operator/OperatorBinary.java
@@ -8,6 +8,13 @@ public class OperatorBinary {
     String nama ="Lutfi Azhar";
     int jumlahSks = 24;
     double indekPrestasiKumulatif = 3.89;
+    public static void main(String[] args) {
 
-
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
 }
