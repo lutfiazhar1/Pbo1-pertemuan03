8e525dfea7254076daf9de9eb6086aa96d2ec1ab
Lutfi Azhar <lutfiazhr12@gmail.com>
2020-04-14 07:00:27 +0700

    pecabangan SwitchExample

---
+++ src/Percabangan/Swichexample.java
@@ -0,0 +1,28 @@
+package Percabangan;
+
+public class Swichexample {
+    public static void main(String[] args) {
+        int Nilai = 'D';
+        switch (Nilai) {
+            case 'A':
+                System.out.println("Mhs = \"Terimakasih pak\"");
+                System.out.println("dosen = \" Selamat ya\"");
+                break;
+            case 'B':
+                System.out.println("Mhs = \"Kenapa Ulun kd dapet Nilai A ?\"");
+                System.out.println("Dosen = \"sarah bapak ai\"");
+                break;
+            case 'C':
+                System.out.println("Mhs = \"ulun turun terus tugas ngumpul ja\" ");
+                System.out.println("Dosen = \"Bisa lah menjawab pas ujian\"");
+                break;
+            case 'D':
+                System.out.println("Mhs = \"ulun turun terus tugal ngumpul ja\"");
+                System.out.println("dosen = \"bujur jua kah ?\"");
+                System.out.println("dosen = \"meriksa berkas \"");
+                System.out.println("dones = \"Menceleng\"");
+                System.out.println("Mhs = \"kavorr \"");
+                break;
+        }
+    }
+}
